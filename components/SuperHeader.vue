46c6d26278c953b3d641e0a91fd63a8c925a2731
ZhanZhynn <zhanzhynn@gmail.com>
2025-07-02 10:30:04 +0800

    setup superheader placeholder and nuxt layout

---
+++ components/SuperHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>This is the super header</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <SuperHeader />
+    <slot />
+  </div>
+</template>
