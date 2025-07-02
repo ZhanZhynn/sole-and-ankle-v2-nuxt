468517242e539afef87638b5572ed9046f274837
ZhanZhynn <zhanzhynn@gmail.com>
2025-07-02 22:51:16 +0800

    add primeicons, default font to segoe ui, superheader

--- components/SuperHeader.vue
+++ components/SuperHeader.vue
@@ -1,7 +1,40 @@
 <template>
-  <div>This is the super header</div>
+  <header>
+    Free Shipping on domestic orders over RM100!
+    <label>
+      <span class="visually-hidden"> Search </span>
+      <IconField>
+        <InputIcon class="pi pi-search" />
+        <InputText v-model="value1" placeholder="Search" size="small" />
+      </IconField>
+    </label>
+    <a href="/">Help</a>
+    <a href="/" class="pi pi-shopping-cart"></a>
+  </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "primeicons/primeicons.css";
 
-<style scoped></style>
+const value1 = ref("");
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  padding: 4px 16px 4px 16px;
+  gap: 16px;
+  align-items: baseline;
+  background-color: black;
+  color: white;
+}
+
+label {
+  margin-left: auto;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
